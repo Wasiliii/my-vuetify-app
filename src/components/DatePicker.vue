32b313f7ee77fdc780352b1b5bcd491062dc6af4
Wasiliii <wasis1999@gmail.com>
2024-12-08 17:23:51 +0300

    date-picker-draft

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,56 @@
+<template>
+        <v-app id="app">
+          <button>
+            +
+          </button>
+        
+          <v-date-picker></v-date-picker>
+             
+         </v-app>
+
+  </template>
+  
+  <script>
+import { createVuetify } from 'vuetify'
+
+export const DatePicker = createVuetify({
+  theme: {
+    defaultTheme: 'light',
+  },
+})
+
+//   export default {
+//     name: 'DatePicker',
+
+//   }
+  </script>
+
+  
+<!-- <script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+    
+  }
+}
+</script> -->
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  </style>
+  
---
+++ src/pages/StartPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <DatePicker />
+</template>
+  
+  <script>
+  import DatePicker from '@/components/DatePicker.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+        DatePicker
+    }
+  }
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
